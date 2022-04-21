4ac6cf3761cba7b127fdd981817907c7163e9bb3
sankajalakshan <39721281+sankajalakshan@users.noreply.github.com>
2022-04-21 21:48:35 +0530

    Update VideoPreview.tsx

--- components/previews/VideoPreview.tsx
+++ components/previews/VideoPreview.tsx
@@ -149,7 +149,7 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
           />
 
           <DownloadButton
-            onClickCallback={() => window.open(`intent:https://onedrive-vercel-index-ten-chi.vercel.app/api/name/All.The.Old.Knives.2022.720p.WEBRip.x264.AAC-%5BFilmsZilla.com%5D.mp4/?path=/All.The.Old.Knives.2022.720p.WEBRip.x264.AAC-%5BFilmsZilla.com%5D.mp4#Intent;package=com.mxtech.videoplayer.ad;end`)}
+            onClickCallback={() => window.open(`intent:https://onedrive-vercel-index-ten-chi.vercel.app/api/name/All.The.Old.Knives.2022.720p.WEBRip.x264.AAC-%5BFilmsZilla.com%5D.mp4#Intent;package=com.mxtech.videoplayer.ad;end`)}
             btnText="MX"
             btnImage="/players/iina.png"
            
